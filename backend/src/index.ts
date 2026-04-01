71728ae52970ada603324815f9a59a29a593b462
Jorge Garzon <A01707102@tec.mx>
2026-04-01 16:38:36 -0600

    [ADD] mongoDB client connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,16 @@ import routes from './interfaces/routes/routes';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { connectDB } from './infrastructure/database/MongoClient';
 
 // Configure .env
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+// MongoDB connection
+await connectDB();
 
 // Start app
 const app = express();
---
+++ backend/src/infrastructure/database/mongoClient.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI as string;
+
+    await mongoose.connect(uri);
+
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+    process.exit(1);
+  }
+};

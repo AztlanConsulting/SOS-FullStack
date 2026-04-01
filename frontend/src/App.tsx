3934eec96fdc07fbf62ca2a7e61c7d0ef1955564
frida <a01707168@tec.mx>
2026-03-31 20:07:27 -0600

    [FIX]architecture files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SOS FullStack</h1>
+      <p>Welcome to the SOS-FullStack-feature-react project!</p>
+    </div>
+  )
+}
+
+export default App

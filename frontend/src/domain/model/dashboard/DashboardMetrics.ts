63f648cacf5dd47fcda93cd75944293054bc3a7d
Mauricio Olguín <86382788+TheSimpleMau@users.noreply.github.com>
2026-04-07 16:47:35 -0600

    [ADD] feature/graphs (#11)
    Thanks!

---
+++ frontend/src/domain/model/dashboard/DashboardMetrics.ts
@@ -0,0 +1,30 @@
+export interface PlanSubscriptionProgress {
+  planName: string;
+  totalDays: number;
+  daysRemaining: number;
+}
+
+export interface PlanDistributionMetric {
+  name: string;
+  value: number;
+  color?: string;
+}
+
+export interface CountryStatsMetric {
+  name: string;
+  value: number;
+}
+
+export interface DailyVisitsMetric {
+  day: number;
+  val: number;
+}
+
+export interface DashboardStats {
+  visits: DailyVisitsMetric[];
+  sales: CountryStatsMetric[];
+  plan: PlanSubscriptionProgress;
+  distribution: PlanDistributionMetric[];
+  countryStats: CountryStatsMetric[];
+  dailyVisits: DailyVisitsMetric[];
+}

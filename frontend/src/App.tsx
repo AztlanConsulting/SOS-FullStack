fd17c66d15a8c043fb9581cc63b7321ea25ddd72
Dongju Mun <dongjumun03@gmail.com>
2026-04-10 11:54:29 -0600

    [ADD] hero section for manuals

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,10 @@
-import { PaymentPage } from './features/payment/components/PaymentPage';
-import { useEffect } from 'react';
-import MapOrganism from '@features/map/components/MapOrganism';
-import { PhotonGeocoding } from '@features/map/services/photonGeocodingService';
 import './App.css';
+import { ManualsHeroSection } from '@features/manuals/components/ManualsHeroSection';
 
 function App() {
-  useEffect(() => {
-    // Warm-up request for improved performance
-    PhotonGeocoding.search('init').catch(() => {});
-  }, []);
-
   return (
     <div className="App">
-      <h1 className="font-bold mb-6">SOS FullStack</h1>
-      <PaymentPage />
-      <h1>SOS FullStack</h1>
-      <p>Welcome to the SOS-FullStack-feature-react project!</p>
-      <MapOrganism />
+      <ManualsHeroSection />
     </div>
   );
 }
---
+++ frontend/src/features/manuals/components/ManualsHeroSection.tsx
@@ -0,0 +1,9 @@
+export const ManualsHeroSection = () => {
+  return (
+    <section className="bg-white w-full">
+      <div className="">
+        <h1 className="">Manuals</h1>
+      </div>
+    </section>
+  );
+};

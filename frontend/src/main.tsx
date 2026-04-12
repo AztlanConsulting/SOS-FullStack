bd4cc1d72bb63031700408822ded3e92a57de2dc
Fridaxca123 <a01707168@itesm.mx>
2026-04-12 10:30:51 -0600

    [ADD]data mode routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Dashboard } from './features/graphs/components/Dashboard';
 import './index.css';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import LandingPage from './presentation/pages/landing/LandingPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />,
   </React.StrictMode>,
 );

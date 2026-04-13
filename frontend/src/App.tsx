adaf908e0fa54957ded2229a4b3f8275d96f1201
Dongju Mun <dongjumun03@gmail.com>
2026-04-12 19:07:50 -0600

    Revert "Fix/landing page (#23)"
    This reverts commit 791a437a12dd5c685a616119bb4030eee8ba778a.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
+import { PaymentPage } from './features/payment/components/PaymentPage';
 import { useEffect } from 'react';
+import MapOrganism from '@features/map/components/MapOrganism';
 import { PhotonGeocoding } from '@features/map/services/photonGeocodingService';
 import './App.css';
-import { RouterProvider } from 'react-router';
-import router from '@routes/router';
 
 function App() {
   useEffect(() => {
@@ -10,7 +10,15 @@ function App() {
     PhotonGeocoding.search('init').catch(() => {});
   }, []);
 
-  return <RouterProvider router={router} />;
+  return (
+    <div className="App">
+      <h1 className="font-bold mb-6">SOS FullStack</h1>
+      <PaymentPage />
+      <h1>SOS FullStack</h1>
+      <p>Welcome to the SOS-FullStack-feature-react project!</p>
+      <MapOrganism />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { Dashboard } from './features/graphs/components/Dashboard';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Dashboard />
   </React.StrictMode>,
 );

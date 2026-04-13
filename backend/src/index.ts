36e7c94933a03f84e627ccec1786242aa70600cb
Jorge Armando Garzón Varela <150094617+jorgeGrVl@users.noreply.github.com>
2026-04-13 15:04:57 -0600

    [ADD] testing setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,13 @@
 import express from 'express';
 import bodyparser from 'body-parser';
-import routes from './interfaces/routes/routes';
-import dotenv from 'dotenv';
-import { mongoDB } from './infrastructure/database/mongoDB/mongoDB';
+import routes from '@interfaces/routes/routes';
 import cors from 'cors';
 
-dotenv.config();
-
-// MongoDB connection
-await mongoDB();
-
-// Start app
 const app = express();
 
-// App configuration
 app.use(cors());
 app.set('trust proxy', true);
 
-// Middleware to capture raw body for Stripe webhook signature verification
 app.use((req, res, next) => {
   if (req.path === '/payments/webhook') {
     let rawBody = Buffer.alloc(0);
@@ -35,21 +25,8 @@ app.use((req, res, next) => {
 
 app.use(bodyparser.json());
 app.set('trust proxy', true);
-// app.use(
-//   cors({
-//     origin: [process.env.FRONT_END_URL ?? 'http://localhost:5173'],
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   }),
-// );
 
 // Routes
 app.use('/', routes);
 
-// Port
-const port = process.env.SERVER_PORT ?? 3000;
-
-// START
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+export default app;

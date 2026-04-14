071efa36acc31b877d3972eade1d548cfd1e1868
Fridaxca123 <a01707168@itesm.mx>
2026-04-13 22:14:12 -0600

    [FIX]siguenos button

--- frontend/src/features/landing/components/HeroSection.tsx
+++ frontend/src/features/landing/components/HeroSection.tsx
@@ -5,13 +5,6 @@ import { HiChevronRight } from 'react-icons/hi';
 const HeroSection = () => {
   return (
     <section className="relative color-secondary-bg overflow-hidden py-8 lg:py-16">
-      <div className="fixed right-0 top-2/3 -translate-y-1/2 z-[1000]">
-        <div className="w-[30px] h-[104px] color-primary-bg rounded-tl-[8px] rounded-bl-[8px] flex items-center justify-center lg:w-[40px] lg:h-[120px]">
-          <span className="text-sm font-medium text-black -rotate-90 whitespace-nowrap tracking-[0.16px]">
-            Síguenos
-          </span>
-        </div>
-      </div>
       <div className="w-5/6 mx-auto">
         <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-8">
           <div className="flex-1 max-w-xl lg:max-w-2xl order-2 lg:order-1 text-left">

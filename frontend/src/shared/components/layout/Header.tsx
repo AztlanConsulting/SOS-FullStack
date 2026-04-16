cc3aeff0ba73b50a55245d627d9d2ff29ec51884
Fridaxca123 <a01707168@itesm.mx>
2026-04-15 22:14:24 -0600

    [FIX]navbar

--- frontend/src/shared/components/layout/Header.tsx
+++ frontend/src/shared/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router';
 import { Bars3Icon } from '@heroicons/react/24/outline';
 import { Text } from '../ui/Text';
 import { LuHouse } from 'react-icons/lu';
@@ -11,7 +12,7 @@ import { CiFacebook } from 'react-icons/ci';
 import { FaInstagram } from 'react-icons/fa';
 import { PiTiktokLogoLight } from 'react-icons/pi';
 import { CiYoutube } from 'react-icons/ci';
-import { FaXTwitter } from "react-icons/fa6";
+import { FaXTwitter } from 'react-icons/fa6';
 
 const navLinks = [
   { label: 'Inicio', href: '/', icon: <LuHouse /> },
@@ -48,6 +49,8 @@ export const socialLinks = [
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSocialOpen, setIsSocialOpen] = useState(false);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   return (
     <header className="bg-white border-b border-(--color-grey-border) py-4 lg:py-[17px] w-full fixed top-0 left-0 right-0 z-50 lg:relative">
@@ -57,7 +60,7 @@ const Header = () => {
             <div
               className="fixed right-0 -translate-y-1/2 z-[1000]"
               style={{
-                top: 'calc(60% - 201px)',
+                top: 'calc(60% - 231px)',
               }}
             >
               <div className="flex flex-col">
@@ -93,17 +96,22 @@ const Header = () => {
         <img src="/1.png" alt="Logo" className="w-12 h-12 lg:w-14 lg:h-14" />
 
         <nav className=" hidden lg:flex items-center gap-8">
-          {navLinks.map((link) => (
-            <a
-              key={link.label}
-              href={link.href}
-              className="hover:text-dark transition-colors"
-            >
-              <Text variant="body" weight="medium">
-                {link.label}
-              </Text>
-            </a>
-          ))}
+          {navLinks.map((link) => {
+            const isActive = pathname === link.href;
+            return (
+              <div
+                key={link.label}
+                onClick={() => navigate(link.href)}
+                className={`hover:text-dark transition-colors cursor-pointer ${
+                  isActive ? 'text-dark border-b-2 border-primary-bg' : ''
+                }`}
+              >
+                <Text variant="body" weight="medium">
+                  {link.label}
+                </Text>
+              </div>
+            );
+          })}
         </nav>
 
         <button
@@ -130,19 +138,41 @@ const Header = () => {
             </div>
 
             {/* Links */}
-            <nav className="flex flex-col gap-6 p-6 flex-1">
-              {navLinks.map((link) => (
-                <a key={link.label} href={link.href}>
-                  <div className="flex gap-2 items-center">
-                    <Text variant="h2" weight="medium" color="text-white">
-                      {link.icon}
-                    </Text>
-                    <Text variant="h2" weight="medium" color="text-white">
-                      {link.label}
-                    </Text>
+            <nav className="flex flex-col gap-6 p-6 flex-1 pr-0">
+              {navLinks.map((link) => {
+                const isActive = pathname === link.href;
+                return (
+                  <div
+                    key={link.label}
+                    onClick={() => {
+                      navigate(link.href);
+                      setIsMenuOpen(false);
+                    }}
+                    className={`flex gap-4 items-center py-3 pl-4 transition-all cursor-pointer ${
+                      isActive ? 'bg-white rounded-l-[20px] shadow' : ''
+                    }`}
+                  >
+                    <div className="flex gap-4 items-center box-shad">
+                      <span
+                        className={
+                          isActive
+                            ? 'text-yellow-400 text-2xl'
+                            : 'text-white text-2xl'
+                        }
+                      >
+                        {link.icon}
+                      </span>
+                      <Text
+                        variant="h2"
+                        weight="medium"
+                        color={isActive ? 'text-yellow-400' : 'text-white'}
+                      >
+                        {link.label}
+                      </Text>
+                    </div>
                   </div>
-                </a>
-              ))}
+                );
+              })}
             </nav>
 
             {/* Bottom button */}

5e28a3f856c241e70dc5cf6cb456391d41034d73
Fridaxca123 <a01707168@itesm.mx>
2026-04-16 18:11:04 -0600

    [FIX]therms

--- frontend/src/pages/Therms.tsx
+++ frontend/src/pages/Therms.tsx
@@ -1,6 +1,6 @@
 import Header from '../shared/components/layout/Header';
 import Footer from '../shared/components/layout/Footer';
-import Therms1 from '@features/landing/components/therms/Therms';
+import ThermsComp from '@features/landing/components/therms/Therms';
 import Conditions from '@features/landing/components/therms/Conditions';
 
 const Therms = () => {
@@ -8,7 +8,7 @@ const Therms = () => {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow pt-[72px] lg:pt-0">
-        <Therms1 />
+        <ThermsComp />
         <Conditions />
       </main>
       <Footer />

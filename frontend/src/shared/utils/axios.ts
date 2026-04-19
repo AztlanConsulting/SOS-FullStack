cc18cf9d7c25e542063427c5f3d5584936c779d8
Jorge Garzon <A01707102@tec.mx>
2026-04-18 22:35:45 -0600

    [ADD] token interceptors for axios instance

--- frontend/src/shared/utils/axios.ts
+++ frontend/src/shared/utils/axios.ts
@@ -1,11 +1,53 @@
 import axios from 'axios';
+import { getAccessToken, setAccessToken } from '@shared/utils/tokenStorage';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 8000,
+  withCredentials: true,
   headers: {
     Accept: 'application/json',
   },
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = getAccessToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await axios.post(
+          `{import.meta.env.VITE_API_BASE_URL}/auth/refresh`,
+          {},
+          { withCredentials: true },
+        );
+
+        const newAccessToken = res.data.accessToken;
+
+        setAccessToken(newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        setAccessToken(null);
+        window.location.href = '/';
+      }
+    }
+  },
+);
 
 export default axiosInstance;

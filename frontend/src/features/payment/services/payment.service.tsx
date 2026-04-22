821a1bde2e975935f19d195415d784d951c09c07
mzapata25 <mateo.zapata.crapa@gmail.com>
2026-04-21 18:54:46 -0600

    [ADD] update front-end for US logic

--- frontend/src/features/payment/services/payment.service.tsx
+++ frontend/src/features/payment/services/payment.service.tsx
@@ -1,12 +1,22 @@
-export const createPaymentIntent = async (amount: number, currency: string) => {
+export const createPaymentIntent = async (
+  amount: number, 
+  currency: string,
+  email: string,
+  petName: string
+) => {
   const res = await fetch(
     `${import.meta.env.VITE_REACT_APP_API_URL}/payments/payment-intent`,
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ amount, currency }),
+      body: JSON.stringify({ amount, currency, email, petName }), 
     },
   );
-  const data: { clientSecret: string | null } = await res.json();
-  return data.clientSecret;
+
+  if (!res.ok) {
+    throw new Error('Error al iniciar el proceso de pago y registro');
+  }
+  const data: { clientSecret: string | null; registrationStarted: boolean } = await res.json();
+  
+  return data;
 };

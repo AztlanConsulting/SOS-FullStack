abb417d3b85113f0633a79e26eb584c99835d95b
SALV <a01707122@tec.mx>
2026-04-23 10:15:08 -0600

    [DELETE] unnecessary log

--- backend/src/use-cases/payments/createPaymentIntent.usecase.ts
+++ backend/src/use-cases/payments/createPaymentIntent.usecase.ts
@@ -8,6 +8,5 @@ export const createPaymentIntent = async (
   paymentProvider: PaymentProvider,
   data: PaymentIntentDTO,
 ): Promise<PaymentIntentResult> => {
-  console.log(data);
   return await paymentProvider.createIntent(data);
 };

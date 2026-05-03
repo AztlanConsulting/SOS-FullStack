9d85f37469925a2786d1984ebe22d3547374d34a
Fridaxca123 <a01707168@itesm.mx>
2026-05-03 17:20:06 -0600

    [FIX] use the upload middleware

--- backend/src/interfaces/routes/clients.routes.ts
+++ backend/src/interfaces/routes/clients.routes.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import multer from 'multer';
 import clientsController from '../controllers/clients.controller';
+import { upload } from '@interfaces/middleware/upload.middleware';
 
 const router = express.Router();
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
 router.post('/publish', clientsController.publishPet);
 

6801cffa701b9e3722833513cf4c6d9a085ca023
Santiago Alducin Villaseñor <145412063+SALV19@users.noreply.github.com>
2026-05-11 18:42:41 -0600

    [ADD] Feature/deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONT_END_URL ?? 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),

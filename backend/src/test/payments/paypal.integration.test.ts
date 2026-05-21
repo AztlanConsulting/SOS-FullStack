58da6999c200e4583a908eb687bbcdae5ea5f1b7
SALV <a01707122@tec.mx>
2026-05-21 00:40:23 -0600

    [FIX] backend tests

--- backend/src/test/payments/paypal.integration.test.ts
+++ backend/src/test/payments/paypal.integration.test.ts
@@ -1,12 +1,15 @@
 import { PaymentModel } from '@/domain/models/payment.model';
 import { PurchaseModel } from '@/domain/models/purchase.model';
+import { WorkshopModel } from '@/domain/models/workshop.model';
 import app from '@/index';
+import { PaymentDataAccess } from '@/infrastructure/data-access/payment.data-access';
 import { clearDatabase, closeDatabase, mongoDB } from '@db/mongoDB/mongoDB';
 import mongoose from 'mongoose';
 import request from 'supertest';
 
 const mockFetch = jest.fn();
 global.fetch = mockFetch;
+
 /**
  * Integration tests for paypal.
  * Focuses on verifying that the repository correctly interacts with the Mongoose model
@@ -24,6 +27,10 @@ describe('Paypal integration tests', () => {
     await mongoDB('test');
   });
 
+  afterEach(async () => {
+    await clearDatabase();
+  });
+
   afterAll(async () => {
     process.env = originalEnv;
     await clearDatabase();
@@ -32,6 +39,17 @@ describe('Paypal integration tests', () => {
 
   describe('Step 1: Create Order', () => {
     it('should create a PayPal order and save a pending record in DB', async () => {
+      // Create a real workshop in DB
+      const workshop = await WorkshopModel.create({
+        name: 'something',
+        description: 'something',
+        price: 50,
+        content: [],
+        category: ['something'],
+        img: { data: Buffer.from('hola'), contentType: 'pdf' },
+        imageUrl: 'http://example.com',
+      });
+
       // 1. Setup Mock Responses for PayPal API
       (global.fetch as jest.Mock)
         .mockResolvedValueOnce({
@@ -47,7 +65,7 @@ describe('Paypal integration tests', () => {
         method: 'paypal',
         customerId: new mongoose.Types.ObjectId().toString(),
         product: {
-          productId: 'work-123',
+          productId: String(workshop._id),
           productName: 'Taller 123',
         },
       };
@@ -123,7 +141,9 @@ describe('Paypal integration tests', () => {
       expect(purchase?.paymentId).toBe('CAPTURE_ID_123');
       expect(purchase?.productId).toBe('workshop_abc');
     });
+  });
 
+  describe('ConfirmPaymentAmount middleware', () => {
     it('should return 500 if the PayPal capture fails', async () => {
       const purchaseDetails = {
         userEmail: 'santiago@example.com',
@@ -141,6 +161,139 @@ describe('Paypal integration tests', () => {
         .send({ purchaseDetails, planId });
 
       expect(response.status).toBe(500);
+    });
+
+    it('should create a PayPal order and save a pending record in DB', async () => {
+      // 1. Setup Mock Responses for PayPal API
+      (global.fetch as jest.Mock)
+        .mockResolvedValueOnce({
+          json: async () => ({ access_token: 'mock_access_token' }),
+        })
+        .mockResolvedValueOnce({
+          json: async () => ({ id: 'PP-ORDER-67890' }),
+        });
+
+      const createPayload = {
+        amount: 50.0,
+        currency: 'MXN',
+        method: 'paypal',
+        customerId: new mongoose.Types.ObjectId().toString(),
+        product: {
+          productId: 'unexisting_id',
+          productName: 'Taller 123',
+        },
+      };
+
+      // 2. Execute Request
+      const response = await request(app)
+        .post('/payments/create-order')
+        .send(createPayload);
+
+      // 3. Assertions
+      expect(response.status).toBe(402);
+    });
+
+    it('calculate price of custom plans', async () => {
+      // 1. Setup Mock Responses for PayPal API
+      (global.fetch as jest.Mock)
+        .mockResolvedValueOnce({
+          json: async () => ({ access_token: 'mock_access_token' }),
+        })
+        .mockResolvedValueOnce({
+          json: async () => ({ id: 'PP-ORDER-67890' }),
+        });
+
+      const paymentSpy = jest.spyOn(PaymentDataAccess, 'createPending');
+
+      const createPayload = {
+        amount: 0.0,
+        currency: 'MXN',
+        method: 'paypal',
+        customerId: new mongoose.Types.ObjectId().toString(),
+        plan: {
+          planName: 'Personalizado',
+          planDetails: {
+            days: 3,
+            km: 14,
+            selectedFeatures: [
+              'Anuncio de 3 días en un área de 10km a la redonda',
+              'Publicación en nuestras redes sociales',
+              'Video y lista de consejos de búsqueda',
+              'Cartel para imprimir',
+            ],
+            totalPrice: 0,
+          },
+        },
+      };
+
+      // 2. Execute Request
+      const response = await request(app)
+        .post('/payments/create-order')
+        .send(createPayload);
+
+      // 3. Assertions
+      expect(response.status).toBe(201);
+      expect(paymentSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          amount: 25.4,
+          clientSecret: 'temp_client_secret',
+          currency: 'MXN',
+          method: 'paypal',
+          orderId: 'PP-ORDER-67890',
+        }),
+      );
+    });
+
+    it('Forged amount', async () => {
+      // Create a real workshop in DB
+      const workshop = await WorkshopModel.create({
+        name: 'something',
+        description: 'something',
+        price: 50,
+        content: [],
+        category: ['something'],
+        img: { data: Buffer.from('hola'), contentType: 'pdf' },
+        imageUrl: 'http://example.com',
+      });
+
+      // 1. Setup Mock Responses for PayPal API
+      (global.fetch as jest.Mock)
+        .mockResolvedValueOnce({
+          json: async () => ({ access_token: 'mock_access_token' }),
+        })
+        .mockResolvedValueOnce({
+          json: async () => ({ id: 'PP-ORDER-67890' }),
+        });
+
+      const paymentSpy = jest.spyOn(PaymentDataAccess, 'createPending');
+
+      const createPayload = {
+        amount: 0.0,
+        currency: 'MXN',
+        method: 'paypal',
+        customerId: new mongoose.Types.ObjectId().toString(),
+        product: {
+          productId: String(workshop._id),
+          productName: 'Taller 123',
+        },
+      };
+
+      // 2. Execute Request
+      const response = await request(app)
+        .post('/payments/create-order')
+        .send(createPayload);
+
+      // 3. Assertions
+      expect(response.status).toBe(201);
+      expect(paymentSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          amount: 50,
+          clientSecret: 'temp_client_secret',
+          currency: 'MXN',
+          method: 'paypal',
+          orderId: 'PP-ORDER-67890',
+        }),
+      );
     });
   });
 });

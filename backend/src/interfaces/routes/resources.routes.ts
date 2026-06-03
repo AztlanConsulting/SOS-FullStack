4e2d56cbd7a8225fa67c9bc99576ea97cff8681e
Mauricio Olguín <86382788+TheSimpleMau@users.noreply.github.com>
2026-06-02 21:02:25 -0600

    [FIX] Add presmissions on resources && [FIX] eslint style (#151)

--- backend/src/interfaces/routes/resources.routes.ts
+++ backend/src/interfaces/routes/resources.routes.ts
@@ -3,11 +3,19 @@ import {
   getResources,
   deleteResourceById,
 } from '../controllers/resource.controller';
-import { authMiddleware, requirePermission } from '../middleware/auth.middleware';
+import {
+  authMiddleware,
+  requirePermission,
+} from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.get('/', getResources);
-router.delete('/:id', authMiddleware, requirePermission('resources', 'delete'), deleteResourceById);
+router.delete(
+  '/:id',
+  authMiddleware,
+  requirePermission('resources', 'delete'),
+  deleteResourceById,
+);
 
 export default router;

87ed9e6f55eb4c44938e634bcf9730d7f4ea6670
Dongju Mun <dongjumun03@gmail.com>
2026-06-04 13:19:21 -0600

    [FIX] emails (#165)

--- frontend/src/features/users/hooks/usePetReportForm.ts
+++ frontend/src/features/users/hooks/usePetReportForm.ts
@@ -232,8 +232,8 @@ export const usePetReportForm = (initialData?: Partial<LostPetReportData>) => {
 
     if (Object.keys(newErrors).length === 0) {
       setLostPetReportData(formData);
-      window.scrollTo(0, 0);
       navigate('/report-confirmation');
+      window.scrollTo(0, 0);
     } else {
       scrollToFirstError(newErrors);
     }
